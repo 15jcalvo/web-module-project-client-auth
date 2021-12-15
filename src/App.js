5337392b56004b61e25bd689e5b635b596be6d4f
James_Calvo <15jcalvo@gmail.com>
2021-12-15 02:33:38 -0500

    friends list component linked and rendered

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login'
+import FriendsList from './components/FriendsList'
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
         </div>
         <Switch>
           <Route path='/login' component={Login} />
+          <Route path='/friends' component={FriendsList} />
           <Route path='/' component={Login} />
         </Switch>
       </div>
---
+++ src/components/FriendsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class FriendsList extends React.Component {
+    render () {
+        return(
+            <div>
+                <h1>friends</h1>
+            </div>
+        )
+    }
+}
+
+export default FriendsList;
