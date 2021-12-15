cabdaca18a63e6bae0dbdc1f73cfaafb51bd2ec1
James_Calvo <15jcalvo@gmail.com>
2021-12-15 02:51:43 -0500

    friends render from axios request

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import axios from 'axios';
+import Friend from './Friend';
 
 class FriendsList extends React.Component {
+    state = {
+        friends: []
+    }
+    componentDidMount() {
+        const token = localStorage.getItem('token')
+        axios.get('http://localhost:9000/api/friends', {
+            headers: {
+                authorization: token
+            }
+        })
+            .then(res=> {
+                console.log(res);
+                this.setState({
+                    friends: res.data
+                })
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    }
     render () {
         return(
             <div>
-                <h1>friends</h1>
+                <h1>Friends</h1>
+                <div>
+                    {this.state.friends.map(friend=> (
+                        <Friend friend={friend}/>
+                    ))}
+                </div>
             </div>
         )
     }
