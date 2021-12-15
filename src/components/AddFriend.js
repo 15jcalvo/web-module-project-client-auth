a923827b3f7e4b20b8ba3a079838546913367c00
James_Calvo <15jcalvo@gmail.com>
2021-12-15 02:59:37 -0500

    add friend form rendered and connected to state

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -1,12 +1,48 @@
 import React from 'react'
+import axios from 'axios'
 
 class AddFriend extends React.Component {
-    render(){
+    state = {
+        friend: {
+          name: '',
+          email: ''
+        }
+      };
+    
+    handleChange = e => {
+        this.setState({
+          friend: {
+            ...this.state.friend,
+            [e.target.name]: e.target.value
+          }
+        });
+      };
+    submit = e => {
+        e.preventDefault();
+        console.log('submit')
+    }
+    render() {
         return(
+        <div>
+            <h1>Add Friend</h1>
             <div>
-                <h1>Add Friend
-                </h1>
+                <form onSubmit={this.submit}>
+                    <input 
+                        type='text'
+                        name='name'
+                        value={this.state.friend.username}
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type='email'
+                        name='email'
+                        value={this.state.friend.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Submit</button>
+                </form>
             </div>
+        </div>
         )
     }
 }
