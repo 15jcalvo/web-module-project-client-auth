358d88ea2b96261e97579486273addf9eb42ce1d
James_Calvo <15jcalvo@gmail.com>
2021-12-15 02:14:32 -0500

    basic dom setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './components/Login'
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+          <div className="header">
+          <h1>Friends Database</h1>
+          <ul>
+            <li>
+              <h2>Login</h2>
+            </li>
+            <li>
+              <h2>Friend List</h2>
+            </li>
+            <li>
+              <h2>Add Friend</h2>
+            </li>
+            <li>
+              <h2>Logout</h2>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/' component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
